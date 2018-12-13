29ac6e4d0a2e52f52712b17b0bf4846dd7bead77
cooperq <cooperq@cooperq.com>
2018-12-13 15:13:00 -0800

    submodule srsLTE EFF fork and create stub for python control script

---
+++ src/crocodilehunter.py
@@ -0,0 +1,14 @@
+def main():
+"""
+* Bootstrap srsUE
+    * test gps
+    * check for board
+    * Load FPGA for hackrf
+    * Start scripts with config file
+* on a loop check db for anomolies
+* generate web interface
+"""
+    print("Crocodile Hunter!")
+
+if __name__ == "__main__":
+    main()
