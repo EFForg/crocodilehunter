7ba111ebc7e3442c013c4600e2e7950171a6b23a
Cooper Quintin <cooperq@eff.org>
2020-02-26 17:40:08 -0800

    add init and install scripts, WIP for #77

---
+++ src/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Installing crocodile hunter"
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+cd `pwd`/../..
+if [ ! -d  /opt ]; then
+  mkdir /opt
+fi
+cp -r crocodilehunter /opt
+cp /opt/crocodilehunter/src/init.d.sh /etc/init.d/crocodilehunter
+chmod +x /etc/init.d/crocodilehunter
+chmod +x /opt/crocodilehunter/src/crocodilehunter.py
