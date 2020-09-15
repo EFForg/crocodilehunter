0ab5ac52635569912b8d474f9d213d7c61ab141f
cooperq <cooperq@cooperq.com>
2020-09-15 11:23:39 -0700

    remove sdr checking from bash script

--- src/bootstrap.sh
+++ src/bootstrap.sh
@@ -9,29 +9,6 @@ if [ ! -f ./config.ini ]; then
     cp config.ini.example config.ini
 fi
 
-# Test for and set up bladeRF
-echo -e "Checking for SDR"
-BLADERF_FPGA_PATH=../data/hostedx40-latest.rbf
-bladeRF-cli -p 1>/dev/null 2>&1
-if [ "$?" -ne "0" ]; then
-    echo -e "No bladeRF devices connected. Assuming UHD device." >&2
-    uhd_find_devices > /dev/null 2>&1
-    if [ $? -ne 0 ]; then
-      echo -e "No UHD device found either"
-      exit 1
-    fi
-else
-    echo -e "BladeRF Found"
-    if [[ `bladeRF-cli -v critical -e version | grep -i "FPGA not loaded"` ]]; then
-      if [[ ! -f $BLADERF_FPGA_PATH ]]; then
-          echo -e "Downloading FPGA for BladeRF"
-          curl https://www.nuand.com/fpga/hostedx40-latest.rbf > $BLADERF_FPGA_PATH
-      fi
-      echo -e "Loading FPGA for BladeRF"
-      bladeRF-cli -v critical -l $BLADERF_FPGA_PATH > /dev/null
-   fi
-fi
-
 # Test GPS
 if [ ! -z $1 ]; then
     echo -e "Skipping GPS test"
@@ -67,7 +44,7 @@ if [ ! -d ./srsLTE/build ]; then
     cd build
     cmake ../ > /dev/null
 else
-    cd srsLTE/build
+    cd srsLTE/build/lib/examples
 fi
 echo -e "Compiling srsUE"
 make > /dev/null
