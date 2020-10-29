dfdd8fd861b41d7863c2e3bb1b295173ebf26413
cooperq <cooperq@cooperq.com>
2020-10-29 10:18:24 -0700

    remove gps setup from bootstrap

--- src/bootstrap.sh
+++ src/bootstrap.sh
@@ -9,34 +9,6 @@ if [ ! -f ./config.ini ]; then
     cp config.ini.example config.ini
 fi
 
-# Test GPS
-if [ ! -z $1 ]; then
-    echo -e "Skipping GPS test"
-else
-    echo -e "Starting GPS"
-    sudo killall -9 gpsd 2> /dev/null
-    sudo service gpsd stop
-    if [ -e /dev/ttyUSB0 ]; then
-        sudo gpsd /dev/ttyUSB0
-        echo -e "Waiting for GPS to sync"
-        TRIES=0
-        until ../experiments/gps.sh | grep -v null  > /dev/null; do
-            echo -e "GPS failed to sync."
-            sleep 1
-            TRIES=$[ $TRIES + 1 ]
-            if [ $TRIES -ge 10 ]; then
-              echo -e "GPS couldn't get a fix, giving up"
-              break;
-            fi
-        done
-        echo -e "GPS successfully got location"
-    else
-        echo -e "No GPS device found"
-        exit 1
-    fi
-fi
-
-
 if [ ! -d ./srsLTE/build ]; then
     echo -e "Building srsLTE for the first time"
     cd srsLTE
